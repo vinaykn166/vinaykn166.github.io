e85197ebdcdd945bf0d60f6f5a680e4786905ce5
Vinay Kumar N <macd50ema@gmail.com.com>
2025-07-22 00:45:43 +0530

    Update homepage content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/social-card.jpeg',
+      image: 'img/docusaurus-social-card.jpeg',
       navbar: {
         title: 'Vinay Kumar N',
         logo: {
